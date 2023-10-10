236cd79d648db2a01edb2000a37b015e7a4726a3
Dimuthu Amaraweera <120401856+DimuthuA@users.noreply.github.com>
2023-10-10 16:29:28 +0530

    Update VideoCard.jsx

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -15,7 +15,7 @@ const VideoCard = ({ video: { id: { videoId }, snippet}}) => {
   // console.log(videoId, snippet);
 
   return (
-    <Card>
+    <Card sx={{ width: { md: '320px', sx: '358px', bexShadow: 'none', borderRadius: '0px'}}}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
           image={snippet?.thumbnails?.high?.url} 
@@ -23,6 +23,19 @@ const VideoCard = ({ video: { id: { videoId }, snippet}}) => {
           sx={{ width: 358, height: 180 }}  
         />
       </Link>
+      <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px', maxWidth: { md: '320px', xs: '358px'}}}>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <Typography variant="subtitle1" fontWeight="bold" color="white">
+          {snippet?.title.slice(0,60) || demoVideoTitle.slice(0,60)}
+        </Typography>
+      </Link>
+      <Link to={snippet?.channelId? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+        <Typography variant="subtitle2" fontWeight="bold" color="gray">
+          {snippet?.channelTitle.slice(0,60) || demoChannelTitle.slice(0,60)}
+          <CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px'}} />
+        </Typography>
+      </Link>
+      </CardContent>
     </Card>
   )
 }
